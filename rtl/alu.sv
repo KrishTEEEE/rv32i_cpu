1ec1807aad695e18d1bd30ecc026bb5cbae69490
KrishTEEEE <sciencekrish7042@gmail.com>
2026-09-26 00:10:10 +0800

    localparams that name alu_control signal for decoding-agnostic microarchitecture

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -4,15 +4,23 @@ module alu(
     input logic [2:0]       alu_control,
     output logic [31:0]     alu_out);
 
+    // Local parameters that name the control signal values based on the operation it maps to
+    // For easier debugging and extension of the processor
+    localparam ADD = 3'd0;
+    localparam SUB = 3'd1;
+    localparam AND = 3'd2;
+    localparam OR = 3'd3;
+    localparam SLTU = 3'd5; //set less than unsigned
+
     // alu_op1 corresponds to rs1 (ins[19:15]), alu_op2 corresponds to rs2 (ins[24:20])
     always_comb begin
         case(alu_control)
-            3'd0: alu_out = alu_op1 + alu_op2;
-            3'd1: alu_out = alu_op1 - alu_op2;
-            3'd2: alu_out = alu_op1 & alu_op2;
-            3'd3: alu_out = alu_op1 | alu_op2;
+            ADD: alu_out = alu_op1 + alu_op2;
+            SUB: alu_out = alu_op1 - alu_op2;
+            AND: alu_out = alu_op1 & alu_op2;
+            OR: alu_out = alu_op1 | alu_op2;
             // 3'd4:;
-            3'd5: alu_out = {31'b0, alu_op1 < alu_op2}; // Unsigned!
+            SLTU: alu_out = {31'b0, alu_op1 < alu_op2}; // Unsigned!
             // 3'd6:;
             // 3'd7:;  
             default: alu_out = 32'b0;
